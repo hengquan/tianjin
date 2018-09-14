794e7337ac63ad7fb50c431200362ad053988513
wanghuiwhwx <wanghui_whwx@sina.com>
2018-09-14 14:54:05 +0800

    环境处理

--- src/main/java/cn/tianjin/unifiedfee/ot/controller/WebDispatchUrl.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/controller/WebDispatchUrl.java
@@ -11,4 +11,21 @@ public class WebDispatchUrl {
         return "/web/index";
     }
 
+    /*学员概况*/
+    @RequestMapping("summary")
+    public String toSummary() {
+        return "/web/summary";
+    }
+
+    /*模拟实操*/
+    @RequestMapping("practice")
+    public String toSimulation() {
+        return "/web/practice";
+    }
+
+    /*在线学习*/
+    @RequestMapping("courseware")
+    public String toOnline() {
+        return "/web/courseware";
+    }
 }
