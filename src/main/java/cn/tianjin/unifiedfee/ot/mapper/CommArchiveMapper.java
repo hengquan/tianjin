39ea772c470e222a29af52e3150cd59dee0bd33e
hengquan <1183998806@qq.com>
2018-09-20 10:45:21 +0800

    ...

--- src/main/java/cn/tianjin/unifiedfee/ot/mapper/CommArchiveMapper.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/mapper/CommArchiveMapper.java
@@ -4,11 +4,18 @@ import java.util.List;
 import java.util.Map;
 
 import cn.tianjin.unifiedfee.ot.entity.CommArchive;
+import cn.tianjin.unifiedfee.ot.entity.KjRefSource;
 
 public interface CommArchiveMapper {
     int insert(CommArchive record);
 
-    int insertSelective(CommArchive record);
-
     public List<CommArchive> getArchiveByObjIds(Map<String, Object> param);
+
+    List<CommArchive> selectByObjId(String objId);
+
+    int update(CommArchive commArchive);
+
+    int delete(CommArchive commArchive);
+
+    KjRefSource get(String id);
 }
