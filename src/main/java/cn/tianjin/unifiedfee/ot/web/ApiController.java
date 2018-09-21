314c1e4fb4c5818f184fb0962bebe8d3df2682a6
hengquan <1183998806@qq.com>
2018-09-21 14:35:34 +0800

    .

--- src/main/java/cn/tianjin/unifiedfee/ot/web/ApiController.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/web/ApiController.java
@@ -313,6 +313,7 @@ public class ApiController {
                     if (ca.getObjId().equals(kj.getId())&&ca.getArchiveType().equals("main")&&m.get("kjUrl")==null) {
                         m.put("kjUrl", ca.getFileUrl());
                     }
+                    m.put("fileName", ca.getFileName());
                 }
             }
             if (m.get("imgUrl")==null) {//默认图片
@@ -320,6 +321,7 @@ public class ApiController {
                 m.put("imgUrl", "");
             }
             if (m.get("kjUrl")==null) m.put("kjUrl", "");
+            if (m.get("fileName")==null) m.put("fileName", "");
             
             retMap.put("returnCode","00");
             retMap.put("data", m);
