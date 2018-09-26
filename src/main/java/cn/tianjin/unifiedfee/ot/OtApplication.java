f7585193f003209bd5129f18772bb9804f4e0bc5
WX <WX@WANGXIA.mshome.net>
2018-09-26 21:19:39 +0800

    代码调整

--- src/main/java/cn/tianjin/unifiedfee/ot/OtApplication.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/OtApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+import cn.tianjin.unifiedfee.ot.logvisit.LogVisitListener;
+
 @SpringBootApplication
 @EnableFeignClients({"cn.taiji","cn.tianjin.unifiedfee"})
 @ComponentScan({"cn.taiji","cn.tianjin.unifiedfee"})
@@ -15,7 +17,10 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ServletComponentScan
 public class OtApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(OtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OtApplication.class, args);
+        System.out.println("=应用启动====================================================");
+        //数据收集器
+        LogVisitListener.begin();
+    }
 }
