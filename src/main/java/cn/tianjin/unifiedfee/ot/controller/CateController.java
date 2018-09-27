09ee6ff232bbcf84738bd323a86e73c4c3e2018c
wanghuiwhwx <wanghui_whwx@sina.com>
2018-09-27 13:15:26 +0800

    修改分类功能

--- src/main/java/cn/tianjin/unifiedfee/ot/controller/CateController.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/controller/CateController.java
@@ -1,5 +1,6 @@
 package cn.tianjin.unifiedfee.ot.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,6 +8,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +17,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.spiritdata.framework.core.model.tree.TreeNode;
+import com.spiritdata.framework.util.DateUtils;
 
 import cn.taiji.oauthbean.dto.UserInfo;
 import cn.taiji.web.base.controller.BaseController;
 import cn.taiji.web.security.UserService;
 import cn.tianjin.unifiedfee.ot.entity.Category;
+import cn.tianjin.unifiedfee.ot.model.CategoryNode;
 import cn.tianjin.unifiedfee.ot.service.CatagoryService;
 import cn.tianjin.unifiedfee.ot.util.HttpPush;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import com.spiritdata.framework.core.model.tree.TreeNodeBean;
 
 @RequestMapping("/cate")
 @Controller
@@ -34,7 +44,6 @@ public class CateController extends BaseController {
     @RequestMapping("insert")
     @ResponseBody
     public Map<String, Object> save(Category cate, HttpServletRequest request, HttpServletResponse response) {
-
         Map<String, Object> retMap = new HashMap<String, Object>();
         try {
             UserInfo ui = userService.getUserInfo();
@@ -52,11 +61,43 @@ public class CateController extends BaseController {
         return retMap;
     }
 
+    @RequestMapping("changeValid")
+    @ResponseBody
+    public Map<String, Object> changeValid(HttpServletRequest request, HttpServletResponse response,
+            @RequestParam(required=false, value="id") String id,
+            @RequestParam(required=false, value="valid", defaultValue="-1") int valid) {
+        Map<String, Object> retMap = new HashMap<String, Object>();
+        try {
+            UserInfo ui = userService.getUserInfo();
+            if (ui == null) {
+                retMap.put("returnCode", "02");
+                retMap.put("messageInfo", "无用户登录");
+                return retMap;
+            }
+            if (StringUtils.isBlank(id)) {
+                retMap.put("returnCode", "03");
+                retMap.put("messageInfo", "分类id为空，无法处理");
+                return retMap;
+            }
+            if (valid==-1) {
+                retMap.put("returnCode", "05");
+                retMap.put("messageInfo", "状态类型无法获得");
+                return retMap;
+            }
+            retMap = categoryService.changeValid(id, valid);
+        } catch (Exception e) {
+            e.printStackTrace();
+            retMap.put("returnCode", "01");
+            retMap.put("messageInfo", e.toString());
+        }
+        return retMap;
+    }
+    
     @RequestMapping("getPageData")
     @ResponseBody
     public Map<String, Object> getPageData(Category cate,
             @RequestParam(value = "offset", defaultValue = "1") int offset,
-            @RequestParam(value = "limit", defaultValue = "3") int limit, HttpServletRequest request,
+            @RequestParam(value = "limit", defaultValue = "10") int limit, HttpServletRequest request,
             HttpServletResponse response) {
         // 跨域
         HttpPush.responseInfo(response);
@@ -65,50 +106,45 @@ public class CateController extends BaseController {
         // 设置page
         PageHelper.offsetPage(offset, limit);
         // 查询数据
-        List<Category> cl = categoryService.getPageData(cate);
-        PageInfo<Category> pageList = new PageInfo<Category>(cl);
-        // if (cl!=null) {
-        // // 放入分页
-        // //PageInfo<Map<String, Object>> pageList=new PageInfo<Map<String,
-        // Object>>(cl);
-        // // 返回
-        // map.put("total", pageList.getTotal());
-        ////
-        //// List<Map<String,Object>> cList=new ArrayList<Map<String,Object>>();
-        //// for (Map<String, Object> _m: cl) {
-        //// Map<String ,Object> newData=new HashMap<String, Object>();
-        //// newData.put("id", (String)_m.get("ID"));
-        //// newData.put("name", (String)_m.get("NAME"));
-        //// newData.put("desc", (String)_m.get("REMARKS"));
-        //// newData.put("parentId",
-        // "0".equals((String)_m.get("PARENT_ID"))?null:(String)_m.get("PARENT_ID"));
-        //// newData.put("parentName", "");
-        //// TreeNode<CategoryNode>
-        // node=(TreeNode<CategoryNode>)categoryService.getCategoryNodeById((String)_m.get("ID"));
-        //// if (node!=null) {
-        //// if (node.getParent()!=null) newData.put("parentName",
-        // node.getParent().getNodeName());
-        //// }
-        //// newData.put("sort", Integer.parseInt(""+_m.get("SORT")));
-        //// newData.put("valid",
-        // (Integer.parseInt(""+_m.get("ISVALID")))==1?"有效":"失效");
-        //// newData.put("createId", (String)_m.get("CREATE_ID"));
-        //// newData.put("createName", (String)_m.get("CREATE_NAME"));
-        //// newData.put("createDate", new
-        // java.sql.Date(((Timestamp)_m.get("CREATE_DATE")).getTime()));
-        //// newData.put("updateDate", new
-        // java.sql.Date(((Timestamp)_m.get("UPDATE_DATE")).getTime()));
-        //// cList.add(newData);
-        //// }
-        //
-        // map.put("rows", pageList.getList());
-        // }
-        // List<Kj> kjs = kjService.getPageData(null);
-        // // 放入分页
-        //// PageInfo<Kj> pageList = new PageInfo<Kj>(kjs);
-        // 返回
-        map.put("total", pageList.getTotal());
-        map.put("rows", pageList.getList());
+        List<Map<String, Object>> cl=categoryService.getPageData(cate);
+        PageInfo<Map<String, Object>> pageList = new PageInfo<Map<String, Object>>(cl);
+        if (cl!=null) {
+            //放入分页
+            List<Map<String,Object>> cList=new ArrayList<Map<String,Object>>();
+            for (Map<String,Object> _m: cl) {
+                Map<String ,Object> newData=new HashMap<String, Object>();
+                newData.put("id", (String)_m.get("ID"));
+                newData.put("name", (String)_m.get("NAME"));
+                newData.put("desc", (String)_m.get("REMARKS"));
+                newData.put("parentId", "0".equals((String)_m.get("PARENT_ID"))?null:(String)_m.get("PARENT_ID"));
+                String pn="";
+                TreeNode<CategoryNode> node=(TreeNode<CategoryNode>)categoryService.getCategoryNodeById((String)_m.get("ID"));
+                if (node!=null) {
+                    TreeNode<? extends TreeNodeBean> pNode=node.getParent();
+                    if (pNode.isRoot()) pn="根";
+                    else pn=node.getParent().getTreePathName("-", 0);
+                }
+                newData.put("parentName", pn);
+                newData.put("sort", Integer.parseInt(""+_m.get("SORT")));
+                newData.put("valid", (Integer.parseInt(""+_m.get("ISVALID")))==1?"有效":"失效");
+                newData.put("createId", (String)_m.get("CREATE_ID"));
+                newData.put("createName", (String)_m.get("CREATE_NAME"));
+                newData.put("updateId", (String)_m.get("UPDATE_ID"));
+                newData.put("updateName", (String)_m.get("UPDATE_NAME"));
+                Date tmp=null;
+                if (null!=_m.get("CREATE_DATE")) {
+                    tmp=new java.sql.Date(((Timestamp)_m.get("CREATE_DATE")).getTime());
+                    newData.put("createDate", DateUtils.convert2LongLocalStr(tmp));
+                }
+                if (null!=_m.get("UPDATE_DATE")) {
+                    tmp=new java.sql.Date(((Timestamp)_m.get("UPDATE_DATE")).getTime());
+                    newData.put("updateDate", DateUtils.convert2LongLocalStr(tmp));
+                }
+                cList.add(newData);
+            }
+            map.put("total", pageList.getTotal());
+            map.put("rows", cList);
+        }
         return map;
     }
 }
