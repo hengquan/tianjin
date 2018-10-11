370525ec512f1b989a7ced68a1bca68a6496f09c
wanghuiwhwx <wanghui_whwx@sina.com>
2018-10-12 06:56:33 +0800

    新的练习已做完

--- src/main/resources/static/tjsrc/js/zxlx.js
+++ src/main/resources/static/tjsrc/js/zxlx.js
@@ -15,26 +15,22 @@ $(function (param) {
   });
   $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +" - " + $( "#slider-range" ).slider( "values", 1 ) );
   // 生成试卷
-  $(".scsj-btn").on("click",function (param) {
-    buildPaper();
-  });
+  $(".scsj-btn").on("click",buildPaper);
   // 提交试卷
-  $(".sub-btn").on("click",function (param) {
-    commitSj();
-  })
+  $(".sub-btn").on("click",commitSj);
 });
 
 function tplCheckbox(res,tmid) {
   var html = ""
   for(var i = 0;i<res.length;i++) {
-   html += '<p><span><input type="checkbox" name="'+tmid+'" value="'+res[i].itemSign+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
+   html += '<p><span><input type="checkbox" disabled=disabled name="'+tmid+'" value="'+res[i].itemSign+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
   }   
   return html
 }
 function tplRadio(res,tmid) {
  var html = ""
   for(var i = 0;i<res.length;i++) {
-   html += '<p><span><input type="radio" name="'+tmid+'" value="'+res[i].itemSign+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
+   html += '<p><span><input type="radio" disabled=disabled name="'+tmid+'" value="'+res[i].itemSign+'"></span>'+res[i].itemSign+'、'+res[i].itemDesc+'</p>'
   }   
   return html
 }
@@ -43,6 +39,8 @@ function buildPaper() {
   layer.msg('试题生成中......', {
     icon: 16 ,shade: 0.01
   });
+  clearInterval(intervalHandler)
+  count = 0
   var _data={
     cateIds:($(".selectpicker").val()).join(","),
     diffRange:$( "#slider-range" ).slider( "values", 0 )+","+$( "#slider-range" ).slider( "values", 1 ),
@@ -60,8 +58,12 @@ function buildPaper() {
       if(res.returnCode == 00) {
         $(".empty").css("display","none")
         $(".lxt").css("display","block")
-        $(".times").css("display","block")
-        $(".sub-btn").css("display","block")
+        $(".times").html("开始练习");
+        $(".times").css("display","block").on("click",start);
+        $(".sub-btn").css("display","block").addClass('disable-btn');
+        $("#table").show();
+        $("#zts").html(res.data.tmList.length);
+        $("#dd").html(0);
         var html = ""
         var zfs = 0
         sjData = res.data
@@ -90,27 +92,29 @@ function buildPaper() {
         $(".zfs").html(zfs)
         $(".zts").html(res.data.tmList.length)
         $(".topic-list").html(html)
-        // 计时开始       
-        intervalHandler=window.setInterval(function() {
-          count++;
-          var s=count%60;
-          var tmp=Math.floor(count/60);
-          var m=tmp%60;
-          tmp=Math.floor(tmp/60);
-          var h=tmp%60;
-          tmp=((100+h)+"").substring(1)+":"+((100+m)+"").substring(1)+":"+((100+s)+"").substring(1);
-          $(".times").html(tmp);
-        }, 1000);
+        // 计时开始
+//        intervalHandler=window.setInterval(function() {
+//          count++;
+//          var s=count%60;
+//          var tmp=Math.floor(count/60);
+//          var m=tmp%60;
+//          tmp=Math.floor(tmp/60);
+//          var h=tmp%60;
+//          tmp=((100+h)+"").substring(1)+":"+((100+m)+"").substring(1)+":"+((100+s)+"").substring(1);
+//          $(".times").html(tmp);
+//        }, 1000);
       } else {
         $("#emptyMsg").html("未生成练习，请重新定义条件");
       }
     }
   });
 }
+
 function start() {
   $("#commitSj").removeClass("disable-btn");
   $("input[type='checkbox']").removeAttr("disabled");
   $("input[type='radio']").removeAttr("disabled");
+  $(".times").css("cursor", "hand").off("click",start);
   intervalHandler=window.setInterval(function() {
     count++;
     var s=count%60;
@@ -119,14 +123,14 @@ function start() {
     tmp=Math.floor(tmp/60);
     var h=tmp%60;
     tmp=((100+h)+"").substring(1)+":"+((100+m)+"").substring(1)+":"+((100+s)+"").substring(1);
-    $(".time-btn").html(tmp);
+    $(".times").html(tmp);
   }, 1000);
 }
 function commitSj() {
-  if($(this).hasClass("disable")) return;
+  if($(this).hasClass("disable-btn")) return;
   clearInterval(intervalHandler)
   count = 0
-  $(this).addClass('disable')
+  $(this).addClass('disable-btn');
   $("input[type='checkbox']").attr("disabled", "disabled");
   $("input[type='radio']").attr("disabled", "disabled");
 
@@ -139,40 +143,41 @@ function commitSj() {
   _data.beginTime=sjData.beginTime;
   _data.endTime=new Date();
   _data.resultType=1;
-  var allAnswer={};
-  
+
+  var allAnswer='';
   var tmList = sjData.tmList
   for(var i = 0;i<tmList.length;i++) {
+    var answer="";
     if (tmList[i].tmType=="判断题" || tmList[i].tmType=="单选题") {
-      allAnswer[tmList[i].tmId]=$('input[name="'+tmList[i].tmId+'"]:checked').val();
+      answer=$('input[name="'+tmList[i].tmId+'"]:checked').val();
     }else {
-      var answer="";
-      allAnswer[tmList[i].tmId]=$('input[name="'+tmList[i].tmId+'"]:checked').val();
       $('input[name="'+tmList[i].tmId+'"]:checked').each(function(){
-        answer+=$(this).val()+",";
+        answer+="#"+$(this).val();
       });
-      answer=answer.substring(0,answer.length-1)
-      allAnswer[tmList[i].tmId]=answer
+      answer=answer.substring(1);
     }
+    if (answer) allAnswer+=","+tmList[i].tmId+":"+answer;
     // var oneTm=tmList[i]
     // $("#tm-result-"+oneTm.tmId).find(".zqda").html("正确答案："+oneTm.tmAnswer)
     // $("#tm-result-"+oneTm.tmId).find(".ndda").html("您的答案："+(!allAnswer[oneTm.tmId] ? "未答" : allAnswer[oneTm.tmId]))
   }
-  _data.answers=allAnswer
+  if (allAnswer) allAnswer=allAnswer.substring(1);
+  _data.answers=allAnswer;
+  console.log(_data);
   $.ajax({
     type: "get",
-    url: "../../demoJson/json142.json",
-    data: "",
+    url: "./train/commitSj",
+    data: _data,
     dataType: "json",
     success:function(res) {
-      console.log(res)
       var oktm = []
       var total = 0
       for(var i = 0;i<res.data.tmList.length;i++) {
         var oneTm=res.data.tmList[i]
         $("#tm-result-"+oneTm.tmId).find(".zqda").html("正确答案："+oneTm.tmAnswer)
-        $("#tm-result-"+oneTm.tmId).find(".ndda").html("您的答案："+(!allAnswer[oneTm.tmId] ? "未答" : allAnswer[oneTm.tmId]))
-        if(oneTm.tmAnswer == allAnswer[oneTm.tmId]) {
+        $("#tm-result-"+oneTm.tmId).find(".ndda").html("您的答案："+(oneTm.answer?oneTm.answer:"未答"));
+        if(oneTm.tmAnswer==oneTm.answer) {
+          $("#tm-result-"+oneTm.tmId).find(".ndda").css("color", "green");
           oktm.push(oneTm)
         }
       }
@@ -180,7 +185,7 @@ function commitSj() {
         total += oktm[i].tmScore
       }
       $(".dd").html(oktm.length)  //答对题数
-      $(".df").html(total) // 得分
+//      $(".df").html(total) // 得分
     }
   })
 }
