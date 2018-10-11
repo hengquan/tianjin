9aaad70693c8b7f823aaf9748de513f759553d05
hengquan <1183998806@qq.com>
2018-10-11 15:35:24 +0800

    ..

--- src/main/java/cn/tianjin/unifiedfee/ot/controller/WebDispatchUrl.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/controller/WebDispatchUrl.java
@@ -35,6 +35,6 @@ public class WebDispatchUrl {
     @RequestMapping("onlineLearning")
     public String toOnlineLearning(HttpServletRequest request) {
         System.out.println("12312312");
-        return "/web/onlineLearning";
+        return "/static/onlineLearning";
     }
 }
