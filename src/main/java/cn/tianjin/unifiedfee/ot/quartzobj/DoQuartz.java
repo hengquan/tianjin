1636f28ea9b4915e90e9e46ff4d468907f7fbb3f
hengquan <1183998806@qq.com>
2018-11-30 12:45:22 +0800

    定时器

--- src/main/java/cn/tianjin/unifiedfee/ot/quartzobj/DoQuartz.java
+++ src/main/java/cn/tianjin/unifiedfee/ot/quartzobj/DoQuartz.java
@@ -1,23 +1,32 @@
 package cn.tianjin.unifiedfee.ot.quartzobj;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
 import com.spiritdata.framework.util.JsonUtils;
 
+import cn.tianjin.unifiedfee.ot.service.AllCompanyService;
 import cn.tianjin.unifiedfee.ot.util.HttpFuncs;
 
 @Service
 public class DoQuartz {
 
+    @Autowired
+    private AllCompanyService allCompanyService;
+
     // 每十秒执行一次
-    @Scheduled(cron = "*/10 * * * * ?")
+    //@Scheduled(cron = "*/10 * * * * ?")
     // 每天23点59执行
-    // @Scheduled(cron = "0 59 23 * * ?")
-    public void work() {
+    @Scheduled(cron = "0 59 23 * * ?")
+    public void work() throws Exception {
         System.out.println("----------------------------------");
         System.out.println("每五秒执行一次");
         System.out.println("----------------------------------");
@@ -27,6 +36,23 @@ public class DoQuartz {
         map.put("pageNum", "1");
         map.put("pageSize", "10000");
         String sendPost = HttpFuncs.sendPost(postUrl, JsonUtils.objToJson(map), false);
-        
+        if (StringUtils.isNotEmpty(sendPost)) {
+            Map<String, Object> result = new HashMap<String, Object>();
+            Gson gson = new Gson();
+            result = gson.fromJson(sendPost, result.getClass());
+            if (result != null) {
+                String success = result.get("success") == null ? "" : result.get("success").toString();
+                if (StringUtils.isNotEmpty(success) && success.equals("true")) {
+                    Map<String, Object> object = (Map<String, Object>) result.get("data");
+                    if (object != null) {
+                        List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
+                        dataList = (List<Map<String, Object>>) object.get("rows");
+                        if (dataList != null && dataList.size() > 0) {
+                            allCompanyService.save(dataList);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
